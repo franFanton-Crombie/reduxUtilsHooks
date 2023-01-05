364d61cb510a21cbd42fcf145dcb7a1b75c7584d
franFanton-4R <francisco.fanton@4rsoluciones.com>
2023-01-05 12:06:19 -0300

    add serach for name and use useMemo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import ThemeContext from '../Context/ThemeContext';
 
 const Header = () => {
     const [darkMode,setDarkMode] = useState(false);
+    const color = useContext(ThemeContext);
     const handleClick = () => {
         setDarkMode(!darkMode)
     }
     return(
         <div className="Header">
-            <h1>ReactHooks</h1>
+            <h1 style={{color}}>ReactHooks</h1>
             <button type="button" onClick={handleClick}>{darkMode ? 'DarkMode' : 'LightMode'}</button>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import ThemeContext from "./Context/ThemeContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeContext.Provider value="red">
+    <ThemeContext.Provider value="blue">
       <App />
     </ThemeContext.Provider>
   </React.StrictMode>
